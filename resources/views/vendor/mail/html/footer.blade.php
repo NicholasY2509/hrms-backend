852e7b6582133668de3df46e1f41477ccaef951e
Nicholas Yang <58143649+NicholasY2509@users.noreply.github.com>
2026-06-05 08:12:25 +0700

    feat: Change Email Design

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -2,8 +2,12 @@
 <td>
 <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
-<td class="content-cell" align="center">
+<td class="content-cell" align="center" style="padding-top: 30px;">
 {{ Illuminate\Mail\Markdown::parse($slot) }}
+<p style="margin-top: 15px; font-size: 11px; color: #9CA3AF; line-height: 1.5;">
+Email ini dihasilkan secara otomatis oleh sistem. Harap tidak membalas email ini.<br>
+<strong>Deltamas HRMS</strong> &copy; {{ date('Y') }}. All rights reserved.
+</p>
 </td>
 </tr>
 </table>
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,16 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo-v2.1.png" class="logo" alt="Laravel Logo">
-@else
-{!! $slot !!}
-@endif
+<table width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="center">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
+{{-- Put your logo file in the "public" folder and name it "logo.png" --}}
+<img src="{{ asset('logo.png') }}" class="logo" alt="{{ strip_tags($slot) }} Logo" style="vertical-align: middle; margin-right: 8px; display: inline-block;" onerror="this.style.display='none'">
+<span style="vertical-align: middle;">{!! $slot !!}</span>
 </a>
+</td>
+</tr>
+</table>
 </td>
 </tr>
