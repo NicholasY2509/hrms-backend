72f82a68d59c80f546fe7c81e6801b9c803a8256
Nicholas Yang <nicholsyoung25@gmail.com>
2026-06-22 10:27:01 +0700

    feat: Team Initial Name

--- app/Modules/Organization/Resources/TeamResource.php
+++ app/Modules/Organization/Resources/TeamResource.php
@@ -21,6 +21,7 @@ class TeamResource extends JsonResource
             'name' => $this->name,
             'work_location_id' => $this->work_location_id,
             'team_head_id' => $this->team_head_id,
+            'team_head_initial_name' => $this->whenLoaded('head', fn() => $this->head->initial_name ?? null),
             'work_location' => new WorkLocationResource($this->whenLoaded('workLocation')),
             'head' => new EmployeeResource($this->whenLoaded('head')),
             'employees' => EmployeeSimpleResource::collection($this->whenLoaded('employees')),
